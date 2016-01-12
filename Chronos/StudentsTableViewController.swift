b312c68e34b0f9a0b8114676546538d681c45340
Carter Harwood <Harwood@users.noreply.github.com>
2016-01-11 21:03:14 -0600

    Reverted StudentTableListView to a basic table. Building from there.

---
+++ Chronos/StudentsTableViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class StudentsTableViewController: UITableViewController {
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LabelCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+}
