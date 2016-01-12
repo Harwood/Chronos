46f7f378739fb9cdb26bc1a93684fa3e53329a40
Carter Harwood <Harwood@users.noreply.github.com>
2016-01-11 21:06:48 -0600

    Updating student stable naming.

--- Chronos/StudentsTableViewController.swift
+++ Chronos/StudentsTableViewController.swift
@@ -2,14 +2,18 @@ import UIKit
 
 class StudentsTableViewController: UITableViewController {
     
-    // MARK: - Table view data source
+    var students = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+        "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+        "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+        "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+        "Pear", "Pineapple", "Raspberry", "Strawberry"]
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
+        return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.students.count
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -17,7 +21,7 @@ class StudentsTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("LabelCell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("StudentCell", forIndexPath: indexPath) 
         
         cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
         
