5574123619f6b15ba024093104d12e18b35f4ede
Carter Harwood <Harwood@users.noreply.github.com>
2016-03-20 14:32:16 -0500

    Init commit for report generation

---
+++ Chronos/PdfAPI.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+/*
+Handles the generation of PDF files
+*/
+final class PdfAPI {
+    static let sharedInstance = PdfAPI()
+
+    private init() {} //This prevents others from using the default '()' initializer for this class.
+}
